a7db5b2e0ff1a9b170c1614efb5375817cffb69c
Operon5 <allan.moloney@hotmail.com>
2024-02-27 19:20:34 -0500

    completed javascript exercise 04 removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,5 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...position) {
+    return array.filter(val => !position.includes(val))
 };
 
 // Do not edit below this line
