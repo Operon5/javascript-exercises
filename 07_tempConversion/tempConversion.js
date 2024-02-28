6589a96498ea857766279f1e89e89310b6c72c0e
Operon5 <allan.moloney@hotmail.com>
2024-02-27 21:08:36 -0500

    completed javascript exercises 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let result1 = (fahrenheit - 32) * 5/9;
+  return Number(result1.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let result2 = (celsius * (9/5) + 32);
+  return Number(result2.toFixed(1));
 };
 
 // Do not edit below this line
