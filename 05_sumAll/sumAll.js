50faff62494f93f2515c675e654d268ee462ada5
Operon5 <allan.moloney@hotmail.com>
2024-02-27 20:28:43 -0500

    completed javascript exercises 05sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,14 @@
-const sumAll = function() {
-
-};
+const sumAll = function(num1, num2) {
+    let min = Math.min(num1, num2);
+    let max = Math.max(num1, num2);
+    if(min < 0) return 'ERROR';
+    if (!Number.isInteger(num1) || !Number.isInteger(num2)) return 'ERROR';
+    let total = 0;
+    for (let i = min; i <= max; i++) {
+        total += i;
+        }
+    return total;
+}
 
 // Do not edit below this line
 module.exports = sumAll;
