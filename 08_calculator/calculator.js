d00cd926b062872c03af79a36e29542273aa0dd5
Operon5 <allan.moloney@hotmail.com>
2024-03-31 14:37:31 -0400

    completed javascript-exercises calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	let addResult = num1 + num2;
+  return addResult
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	let subResult = num1 - num2;
+  return subResult;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let sumResult = arr.reduce(function (x,y) {
+    return x + y;
+  }, 0);
+  return sumResult;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let multiplyResult = 1;
+  for (let i = 0; i < arr.length; i++) {
+    multiplyResult *= arr[i];
+  }
+  return multiplyResult;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	let powerResult = num1 ** num2;
+  return powerResult;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let factorialResult = 1;
+  while (num > 0) {
+    factorialResult *= num;
+    num--;
+  }
+  return factorialResult;
 };
 
 // Do not edit below this line
