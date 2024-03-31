cd3106138655ec801f799433e36d37bc01ee23ca
Operon5 <allan.moloney@hotmail.com>
2024-03-31 16:00:14 -0400

    completed javascript exercises palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const charRemove = /[\W_]/g;
+    const newStr = str.toLowerCase().replace(charRemove, '');
+    const reverseStr = newStr.split('').reverse().join('');
+    if (reverseStr === newStr) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
